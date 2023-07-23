4a34f09a66032de01bd55f727a8d79861a44d7ab
Arxoto <1484663586@qq.com>
2023-07-23 13:45:00 +0800

    修改icon

--- src/page/Default.tsx
+++ src/page/Default.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import reactLogo from "../assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 
 export default function () {
@@ -17,13 +16,13 @@ export default function () {
 
       <div className="row">
         <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+          <img src="/wom.png" className="logo vite" alt="Vite logo" />
         </a>
         <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+          <img src="/wow.png" className="logo tauri" alt="Tauri logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src="cursor.png" className="logo react" alt="React logo" />
         </a>
       </div>
 
