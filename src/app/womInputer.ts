9e861c31a25139d58651153657c3fbeffa403d6a
Arxoto <1484663586@qq.com>
2023-08-27 23:25:02 +0800

    完善匹配条件【等价、开头、包含、散列】

--- src/app/womInputer.ts
+++ src/app/womInputer.ts
@@ -15,7 +15,7 @@ const itemsInit = () => {
             const { numb: nA } = getItemTypeId(a.theType);
             const { numb: nB } = getItemTypeId(b.theType);
             return nA !== nB ? nA - nB : a.id - b.id;
-        }).map(item => ({...item, theKey: item.title.toLowerCase()}));
+        }).map(item => ({ ...item, theKey: item.title.toLowerCase() }));
         console.log(itemsCache);
 
     });
@@ -89,20 +89,44 @@ function searchItems(input: Input): ItemDescriptor[] {
     return formated.flat();
 }
 
-const matchItemsBase = (fn: (input: Input, tmpItems: ItemReduced[]) => ItemReduced[]) => (input: Input, tmpItems: ItemReduced[]) => {
+// todo 根据 hasArg 决定过滤逻辑 可以直接加载fn后面
+// 都是浅拷贝 所以能直接for-each修改状态
+const matchItems = (fn: (input: Input, tmpItems: ItemReduced[]) => ItemReduced[]) => (input: Input, tmpItems: ItemReduced[]) => {
     let result = fn(input, tmpItems.filter(item => !item.selected));
     result.forEach(item => item.selected = true);
     return result;
 };
 
 const matchFns = [
-    // todo 根据 hasArg 决定过滤逻辑
-    matchItemsBase((input: Input, tmpItems: ItemReduced[]) =>
-        tmpItems.filter(item => item.title === input.key)
+    // equals
+    matchItems((input: Input, tmpItems: ItemReduced[]) =>
+        tmpItems.filter(item => item.theKey === input.key)
     ),
-    // 若空格 这里会全部显示（匹配''）
-    matchItemsBase((input: Input, tmpItems: ItemReduced[]) =>
-        tmpItems.filter(item => item.title.startsWith(input.key))
+    // startsWith  若空格 这里会全部显示（匹配''）
+    matchItems((input: Input, tmpItems: ItemReduced[]) =>
+        tmpItems.filter(item => item.theKey.startsWith(input.key))
+    ),
+    // includes
+    matchItems((input: Input, tmpItems: ItemReduced[]) =>
+        tmpItems.filter(item => item.theKey.includes(input.key))
+    ),
+    // custom match
+    matchItems((input: Input, tmpItems: ItemReduced[]) =>
+        tmpItems.filter(item => {
+            const inKey = input.key;
+            const itKey = item.theKey;
+            if (inKey.length >= itKey.length) {
+                return false;
+            }
+            let n = 0, t = 0;
+            while (n < inKey.length && t < itKey.length) {
+                if (inKey[n] === itKey[t]) {
+                    n++;
+                }
+                t++;
+            }
+            return n === inKey.length;
+        })
     ),
 ];
 
