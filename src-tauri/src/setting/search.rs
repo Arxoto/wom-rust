a148fa83183c13cbe4b89d39252bc3664828091e
Arxoto <1484663586@qq.com>
2024-02-13 15:11:55 +0800

    icon命名调整和item存放位置的思考

--- src-tauri/src/setting/search.rs
+++ src-tauri/src/setting/search.rs
@@ -21,19 +21,19 @@ fn find_match(item: &ItemCommon, keyword: &str) -> bool {
                 if let Some(t) = target.next() {
                     if s == t {
                         continue 'outer;
-                    } else {
-                        continue;
                     }
-                } else {
-                    return false;
+                    continue;
                 }
+                return false;
             }
-        } else {
-            return true;
         }
+        return true;
     }
 }
 
+/// 在后端保存实例和查询
+/// 优点：保证在app维度全局唯一 不会因新建窗口/页面切换导致重新载入设置
+/// 缺点：内存占用变大（后端和前端数据是独立的） 无法通过trigger函数变量定制化item
 pub fn search(items: &[ItemCommon], keyword: &str, has_args: bool) -> Vec<ItemCommon> {
     let funcs = vec![find_equal, find_starts_with, find_contains, find_match];
 
--- src/pages/wom/item/ItemIcon.tsx
+++ src/pages/wom/item/ItemIcon.tsx
@@ -1,10 +1,10 @@
 import { ItemType } from "../../../core/womItem";
 
-import plugin from "../../../assets/plugin.svg";
 import cmd from "../../../assets/cmd.svg";
 import web from "../../../assets/web.svg";
 import folder from "../../../assets/folder-2.svg";
-import app from "../../../assets/app.svg";
+import files from "../../../assets/app-1.svg";
+import app from "../../../assets/app-2.svg";
 import more from "../../../assets/more.svg";
 
 interface ItemIconArgs {
@@ -13,16 +13,14 @@ interface ItemIconArgs {
 
 export default function ({ itemType }: ItemIconArgs) {
     switch (itemType) {
-        case ItemType.Plugin:
-            return <img src={more} alt="" style={{ width: '50%' }} />
         case ItemType.Cmd:
             return <img src={cmd} alt="" style={{ width: '75%' }} />;
         case ItemType.Web:
             return <img src={web} alt="" style={{ width: '65%' }} />;
-        case ItemType.Files:
-            return <img src={plugin} alt="" style={{ width: '75%' }} />;
         case ItemType.Folder:
             return <img src={folder} alt="" style={{ width: '55%' }} />;
+        case ItemType.Files:
+            return <img src={files} alt="" style={{ width: '75%' }} />;
         case ItemType.App:
             return <img src={app} alt="" style={{ width: '65%' }} />;
         default:
